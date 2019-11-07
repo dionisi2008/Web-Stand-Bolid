2262f8808b2b324d0d5d36b9f8e55683b82eae15
dionisi2008 <dionisi2008@gmail.com>
2019-11-07 18:12:00 +0700

    Create Prodject

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Web_Stand_Bolid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
