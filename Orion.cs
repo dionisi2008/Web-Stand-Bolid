1684bb28ea151fd541ecfb5078fd16393664b9a1
dionisi2008 <dionisi2008@gmail.com>
2019-11-10 21:37:22 +0700

    Расчисал библеотеку Bolid

--- Orion.cs
+++ Orion.cs
@@ -1,154 +1,201 @@
 using System.Timers;
-public class Bolid
+
+namespace Bolid
 {
-    public class PrioborOrion
+    namespace Deviecs
     {
-        public Bolid.PriborСomponents.Power Power;
-        public Bolid.PriborСomponents.RS485 RS485;
-        public Bolid.PriborСomponents.PPZY PPZY;
-        public Bolid.PriborСomponents.Tamper Tamper;
-        public Bolid.PriborСomponents.Indicator IndicatorWork;
-    }
-    public class EvetsPribors
-    {
-        public enum PowerEvents {АварияИсточникаПитания, НормаИсточникаПитания};
-    }
-    public class PriborsRadialLoops : PrioborOrion
-    {
-        
-        public Bolid.PriborСomponents.Indicator[] IndicatorsReleay;
-        public Bolid.PriborСomponents.Shleif[] Shleif;
-        public Bolid.PriborСomponents.Indicator[] IndicatorShleif;
-        public Bolid.PriborСomponents.Key[] KeysShleif;
-        }
-    public class PriborСomponents
-    {
-
-        
-        public class Indicator
+        namespace ComponentsDevice
         {
-            public Timer AllForTime;
-            public Timer OnTime;
-            public bool StateIndicator = false;
-            public Indicator(bool GetState)
+            namespace Indicator
             {
-                this.StateIndicator = GetState;
-                System.Console.WriteLine("Work Indicator Start");
-            }
-
-            public Indicator(double GetAllForTime, double GetOnTime, bool StartStete)
-            {                                
-                AllForTime = new Timer(GetAllForTime);
-                OnTime = new Timer(GetOnTime);
-                AllForTime.Elapsed += FunctionAllTimerPick;
-                OnTime.Elapsed += FunctionOnTimerPick;
-                this.StateIndicator = StartStete;
-                AllForTime.Start();
-            }
-
-            public void FunctionAllTimerPick(object sender, ElapsedEventArgs e)
-            {
-                System.Console.WriteLine("Work Indicator Start");
-                OnTime.Start();
-            }
-
-            public void FunctionOnTimerPick(object sender, ElapsedEventArgs e) 
-            {
-                System.Console.WriteLine("Work Indicator Stop");
-                this.StateIndicator = true;
-                this.OnTime.Stop();                
-            }
-            
-        }
-        public class RS485
-        {
-
-        }
-        public class RS232
-        {
-
-        }
-        public class Shleif
-        {
-            public double KResits;
-            public int ACP;
-        }
-        public class Reley
-        {
-
-        }
-        public class Power
-        {
-            public delegate void OutStatePower(Bolid.EvetsPribors.PowerEvents State);
-            public event OutStatePower MessageCPPower;
-            public double StateVolt = 0;
-            public Power(double GetV)
-            {
-                StateVolt = GetV;
-            }
-
-            public Power()
-            {
-                
-            }
-
-            public void ConnctPower(double GetV)
-            {                
-                StateVolt = GetV;
-                if (StateVolt < VMinimum)
+                public class Indicator
                 {
-                    MessageCPPower(Bolid.EvetsPribors.PowerEvents.АварияИсточникаПитания);
+                    public Timer AllForTime;
+                    public Timer OnTime;
+                    public bool StateIndicator = false;
+                    public Indicator(bool GetState)
+                    {
+                        this.StateIndicator = GetState;
+                        System.Console.WriteLine("Work Indicator Start");
+
+                    }
+
+                    public Indicator(double GetAllForTime, double GetOnTime, bool StartStete)
+                    {
+                        AllForTime = new Timer(GetAllForTime);
+                        OnTime = new Timer(GetOnTime);
+                        AllForTime.Elapsed += FunctionAllTimerPick;
+                        OnTime.Elapsed += FunctionOnTimerPick;
+                        this.StateIndicator = StartStete;
+                        AllForTime.Start();
+                    }
+
+                    public void FunctionAllTimerPick(object sender, ElapsedEventArgs e)
+                    {
+                        System.Console.ForegroundColor = System.ConsoleColor.Green;
+                        System.Console.WriteLine("Work Indicator Start");
+                        OnTime.Start();
+                    }
+
+                    public void FunctionOnTimerPick(object sender, ElapsedEventArgs e)
+                    {
+                        System.Console.ForegroundColor = System.ConsoleColor.Black;
+                        System.Console.WriteLine("Work Indicator Stop");
+                        this.StateIndicator = true;
+                        this.OnTime.Stop();
+                    }
+
                 }
-                else if (StateVolt > VMinimum)
-                {
-                    MessageCPPower(Bolid.EvetsPribors.PowerEvents.АварияИсточникаПитания);
-                }
-                else
-                {
-                    MessageCPPower(Bolid.EvetsPribors.PowerEvents.НормаИсточникаПитания);
-                }
+
             }
-            public double VMinimum;
-            public double VMaximum;
-        }
-        public class WorkTemperture
-        {
-
-        }
-        public class Tamper
-        {
-
-        }
-        public class PPZY
-        {
-
-        }
-        public class CP
-        {
-            public class Programms
+            namespace RS485
             {
-                public class ProgrammReley
+                public class RS485
                 {
 
                 }
 
-                public class ProgrammIndicators
+            }
+            namespace RS232
+            {
+                public class RS232
                 {
-                    
+
                 }
 
-                public class ProgrammSheif
+            }
+            namespace Shleif
+            {
+                public class Shleif
                 {
-                    
+                    public double KResits;
+                    public int ACP;
                 }
+
+            }
+            namespace Reley
+            {
+                public class Reley
+                {
+
+                }
+
+            }
+            namespace Power
+            {
+                public enum PowerStates { АварияИсточникаПитания, НормаИсточникаПитания };
+                public class Power
+                {
+                    public delegate void OutStatePower(Bolid.Deviecs.ComponentsDevice.Power.PowerStates State);
+                    public event OutStatePower MessageCPPower;
+                    public double StateVolt = 0;
+                    public Power(double GetV)
+                    {
+                        StateVolt = GetV;
+                    }
+
+                    public Power()
+                    {
+
+                    }
+
+                    public void ConnctPower(double GetV)
+                    {
+                        StateVolt = GetV;
+                        if (StateVolt < VMinimum)
+                        {
+                            MessageCPPower(Bolid.Deviecs.ComponentsDevice.Power.PowerStates.АварияИсточникаПитания);
+                        }
+                        else if (StateVolt > VMinimum)
+                        {
+                            MessageCPPower(Bolid.Deviecs.ComponentsDevice.Power.PowerStates.АварияИсточникаПитания);
+                        }
+                        else
+                        {
+                            MessageCPPower(Bolid.Deviecs.ComponentsDevice.Power.PowerStates.НормаИсточникаПитания);
+                        }
+                    }
+                    public double VMinimum;
+                    public double VMaximum;
+                }
+
+            }
+            namespace WorkTemperture
+            {
+                public class WorkTemperture
+                {
+
+                }
+
+            }
+            namespace Tamper
+            {
+                public class Tamper
+                {
+
+                }
+
+            }
+            namespace PPZY
+            {
+                public class PPZY
+                {
+
+
+                }
+
+            }
+            namespace CP
+            {
+                public class CP
+                {
+                    public class Programms
+                    {
+                        public class ProgrammReley
+                        {
+
+                        }
+
+                        public class ProgrammIndicators
+                        {
+
+                        }
+
+                        public class ProgrammSheif
+                        {
+
+                        }
+                    }
+                }
+
+            }
+            namespace Key
+            {
+                public class Key
+                {
+
+                }
+
             }
         }
-        public class Key
+        namespace RadialLoops
         {
-
+            public class PriborsRadialLoops : Bolid.Deviecs.Priobor
+            {
+                public Bolid.Deviecs.ComponentsDevice.Indicator.Indicator[] IndicatorsReleay;
+                public Bolid.Deviecs.ComponentsDevice.Shleif.Shleif[] Shleif;
+                public Bolid.Deviecs.ComponentsDevice.Indicator.Indicator[] IndicatorShleif;
+                public Bolid.Deviecs.ComponentsDevice.Key.Key[] KeysShleif;
+            }
         }
-    }
-    public class Chips
-    {
+
+        public class Priobor
+        {
+            public Bolid.Deviecs.ComponentsDevice.Power.Power Power;
+            public Bolid.Deviecs.ComponentsDevice.RS485.RS485 RS485;
+            public Bolid.Deviecs.ComponentsDevice.PPZY.PPZY PPZY;
+            public Bolid.Deviecs.ComponentsDevice.Tamper.Tamper Tamper;
+            public Bolid.Deviecs.ComponentsDevice.Indicator.Indicator IndicatorWork;
+        }
     }
 }
--- Signal20.cs
+++ Signal20.cs
@@ -1,27 +1,28 @@
 using System;
 using System.Collections.Generic;
 
-public class  Signal20 : Bolid.PriborsRadialLoops
+public class  Signal20 : Bolid.Deviecs.RadialLoops.PriborsRadialLoops
 {
-    public delegate void GetEventPowerDelegate(Bolid.EvetsPribors.PowerEvents GetEvent);
+  
+    public delegate void GetEventPowerDelegate(Bolid.Deviecs.ComponentsDevice.Power.PowerStates GetEvent);
     public event GetEventPowerDelegate OutEventPower;
     public Signal20()
     {
-        this.Power = new Bolid.PriborСomponents.Power();
+        this.Power = new Bolid.Deviecs.ComponentsDevice.Power.Power();
         this.Power.MessageCPPower += ProgressEventPower;
         Power.VMaximum = 28;
         Power.VMinimum = 10.2;
     }
 
-    public void ProgressEventPower(Bolid.EvetsPribors.PowerEvents GetEvent)
+    public void ProgressEventPower(Bolid.Deviecs.ComponentsDevice.Power.PowerStates GetEvent)
     {
         switch (GetEvent)
         {
-            case Bolid.EvetsPribors.PowerEvents.АварияИсточникаПитания:
-                this.IndicatorWork = new Bolid.PriborСomponents.Indicator(1000, 125, true);
+            case Bolid.Deviecs.ComponentsDevice.Power.PowerStates.АварияИсточникаПитания:
+                this.IndicatorWork = new Bolid.Deviecs.ComponentsDevice.Indicator.Indicator(1000, 125, true);
             break;
-            case Bolid.EvetsPribors.PowerEvents.НормаИсточникаПитания:
-                this.IndicatorWork = new Bolid.PriborСomponents.Indicator(true); 
+            case Bolid.Deviecs.ComponentsDevice.Power.PowerStates.НормаИсточникаПитания:
+                this.IndicatorWork = new Bolid.Deviecs.ComponentsDevice.Indicator.Indicator(true); 
                 //Переход к фунции чтения из памяти
             break;
         }
